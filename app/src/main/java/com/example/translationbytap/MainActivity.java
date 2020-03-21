6a8d1560e559dad6ac937383d07748ea34f76ed8
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-21 11:00:17 +0600

    setting tap function

--- app/src/main/java/com/example/translationbytap/MainActivity.java
+++ app/src/main/java/com/example/translationbytap/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView arshadImage;
-
+    Button buttonFullScreen;
     int[] stateVales;
     int index;
 
